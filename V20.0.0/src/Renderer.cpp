078c245def8746ece7a4fa67e42e791f427f094d
olie-ver <oliverlie@krystal3d.com>
2026-05-21 13:07:14 -0700

    Added verbosity flags

--- V20.0.0/src/Renderer.cpp
+++ V20.0.0/src/Renderer.cpp
@@ -16,6 +16,15 @@ namespace internal {
                 case Core::Verbosity::Minimum:
                     renderMinimum(testRun);
                     break;
+                case Core::Verbosity::PassOnly:
+                    renderPassOnly(testRun);
+                    break;
+                case Core::Verbosity::FailOnlyMin:
+                    renderFailMin(testRun);
+                    break;
+                case Core::Verbosity::FailOnlyAll:
+                    renderFailAll(testRun);
+                    break;
             }
 
             std::cout << std::string(50, '-') << std::endl;
@@ -26,12 +35,47 @@ namespace internal {
             std::cout << "Time: " << testRun.totalMs << " ms" << std::endl;
         }
 
-        void ConsoleRenderer::renderAll(Core::TestRun& testRun) {
-            renderDefault(testRun);
+        void ConsoleRenderer::renderDefault(Core::TestRun& testRun) {
+            auto& tests = testRun.results;
+
+            for (auto&& it = tests.begin(); it != tests.end(); ++it) 
+            {
+                const std::vector<Core::TestResult>& test = it->second;
+                size_t size = test.size();
+                for (size_t i = 0; i < size; i++) {
+                    const Core::TestResult& result = test[i];
+
+                    switch (result.status) {
+                        case Core::TestStatus::Passed:
+                            std::cout << "[PASS] " << result.suiteName << " -> " << result.testName;
+                            std::cout << " (" << result.durationMs << " ms" << ')' << std::endl;
+                            testRun.passed++;
+                            break;
+                    
+                        case Core::TestStatus::Failed:
+                            std::cout << "[FAIL] " << result.suiteName << " -> " << result.testName;
+                            std::cout << " (" << result.durationMs << " ms" << ')' << std::endl;
+
+                            for (size_t j = 0; j < result.failures.size(); j++) {
+                                const Core::Failure& fail = result.failures[j];
+                                std::cout << "\tat: " << fail.file << ":" << fail.line << std::endl;
+                                std::cout << '\t' << fail.message << std::endl;
+                                std::cout << std::endl;
+                            }
+                            testRun.failed++;
+                            break;
+
+                        case Core::TestStatus::Skipped:
+                            std::cout << "[SKIP] " << result.suiteName << " -> " << result.testName << std::endl;
+                            testRun.skipped++;
+                            break;
+                    }
+                }
+            }
         }
 
         void ConsoleRenderer::renderMinimum(Core::TestRun& testRun) {
-            std::map<std::string, std::vector<Core::TestResult>>& tests = testRun.results;
+            auto& tests = testRun.results;
             for (auto&& it = tests.begin(); it != tests.end(); ++it) 
             {
                 const std::vector<Core::TestResult>& test = it->second;
@@ -61,42 +105,81 @@ namespace internal {
             }
         }
 
-        void ConsoleRenderer::renderVerbose(Core::TestRun& testRun) {
-            renderDefault(testRun);
-        }
-
-        void ConsoleRenderer::renderDefault(Core::TestRun& testRun) {
-            std::map<std::string, std::vector<Core::TestResult>>& tests = testRun.results;
-
+        void ConsoleRenderer::renderPassOnly(Core::TestRun& testRun) {
+            auto& tests = testRun.results;
             for (auto&& it = tests.begin(); it != tests.end(); ++it) 
             {
                 const std::vector<Core::TestResult>& test = it->second;
                 size_t size = test.size();
                 for (size_t i = 0; i < size; i++) {
                     const Core::TestResult& result = test[i];
-
                     switch (result.status) {
-                        case Core::TestStatus::Passed:
+                        case Core::TestStatus::Passed: 
                             std::cout << "[PASS] " << result.suiteName << " -> " << result.testName;
                             std::cout << " (" << result.durationMs << " ms" << ')' << std::endl;
                             testRun.passed++;
                             break;
-                    
+                        case Core::TestStatus::Failed:
+                            testRun.failed++;
+                            break;
+                        case Core::TestStatus::Skipped:
+                            testRun.skipped++;
+                            break;
+                    }
+                }
+            }
+        }
+
+        void ConsoleRenderer::renderFailAll(Core::TestRun& testRun) {
+            auto& tests = testRun.results;
+            for (auto&& it = tests.begin(); it != tests.end(); ++it) 
+            {
+                const std::vector<Core::TestResult>& test = it->second;
+                size_t size = test.size();
+                for (size_t i = 0; i < size; i++) {
+                    const Core::TestResult& result = test[i];
+                    switch (result.status) {
+                        case Core::TestStatus::Passed: 
+                            testRun.passed++;
+                            break;
                         case Core::TestStatus::Failed:
                             std::cout << "[FAIL] " << result.suiteName << " -> " << result.testName;
                             std::cout << " (" << result.durationMs << " ms" << ')' << std::endl;
 
                             for (size_t j = 0; j < result.failures.size(); j++) {
                                 const Core::Failure& fail = result.failures[j];
-                                std::cout << std::string(4, ' ') << "at: " << fail.file << ":" << fail.line << std::endl;
-                                std::cout << std::string(4, ' ') << fail.message << std::endl;
+                                std::cout << "\tat: " << fail.file << ":" << fail.line << std::endl;
+                                std::cout << '\t' << fail.message << std::endl;
                                 std::cout << std::endl;
                             }
                             testRun.failed++;
                             break;
-
                         case Core::TestStatus::Skipped:
-                            std::cout << "[SKIP] " << result.suiteName << " -> " << result.testName << std::endl;
+                            testRun.skipped++;
+                            break;
+                    }
+                }
+            }
+        }
+
+        void ConsoleRenderer::renderFailMin(Core::TestRun& testRun) {
+            auto& tests = testRun.results;
+            for (auto&& it = tests.begin(); it != tests.end(); ++it) 
+            {
+                const std::vector<Core::TestResult>& test = it->second;
+                size_t size = test.size();
+                for (size_t i = 0; i < size; i++) {
+                    const Core::TestResult& result = test[i];
+                    switch (result.status) {
+                        case Core::TestStatus::Passed: 
+                            testRun.passed++;
+                            break;
+                        case Core::TestStatus::Failed:
+                            std::cout << "[FAIL] " << result.suiteName << " -> " << result.testName;
+                            std::cout << " (" << result.durationMs << " ms" << ')' << std::endl;
+                            testRun.failed++;
+                            break;
+                        case Core::TestStatus::Skipped:
                             testRun.skipped++;
                             break;
                     }
